58020ea00c4bb74ced27e125df884f440ac0f590
tsiguenz <tsiguenz@student.42.fr>
2024-04-04 18:13:18 +0200

    add level08 source code and readme

--- level08/README.md
+++ level08/README.md
@@ -1,4 +1,52 @@
-level08@OverRide:/tmp$ mkdir -p  backups/home/users/level09
+# Walkthrough level08
+
+Try to run the binary:
+
+```bash
+level08@OverRide:~$ ./level08 
+Usage: ./level08 filename
+ERROR: Failed to open (null)
+level08@OverRide:~$ ./level08 toto
+ERROR: Failed to open toto
+```
+
+We need to give a valid file:
+
+```bash
+level08@OverRide:~$ echo toto > tata
+-bash: tata: Permission denied
+```
+
+We don't have the permission, try to do it in `/tmp`:
+
+```bash
+level08@OverRide:~$ echo toto > /tmp/tata
+level08@OverRide:~$ ./level08 /tmp/tata
+ERROR: Failed to open ./backups//tmp/tata
+```
+
+The binary try to create a file with `./backups` before our filename.  
+So we try to create a directory named `tmp` in the `backups` directory:
+
+```bash
+level08@OverRide:/tmp$ mkdir backups
+level08@OverRide:/tmp$ /home/users/level08/level08 tata 
+level08@OverRide:/tmp$ cat backups/tata 
+toto
+```
+
+This is working, try with the flag:
+
+```bash
+level08@OverRide:/tmp$ /home/users/level08/level08 /home/users/level09/.pass
+ERROR: Failed to open ./backups//home/users/level09/.pass
+```
+
+We can't open `./backups//home/users/level09/.pass` because the directories does'nt exist.
+
+```bash
+level08@OverRide:/tmp$ mkdir -p backups/home/users/level09
 level08@OverRide:/tmp$ /home/users/level08/level08 /home/users/level09/.pass
 level08@OverRide:/tmp$ cat backups/home/users/level09/.pass
 fjAwpJNs2vvkFLRebEvAQ2hFZ4uQBWfHRsP62d8S
+```
--- level08/source.c
+++ level08/source.c
@@ -1,17 +1,32 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void log_wrapper(FILE *backup, char *str, char *input) {
+  char dest[264];
+  strcpy(dest, str);
+  snprintf(&dest[strlen(dest)], strlen(input), input);
+  dest[strcspn(dest, "\n")] = '\0';
+  fprintf(backup, "LOG: %s\n", dest);
+}
 
 int main(int ac, char **av) { // rbp-0x94 and rbp-0xa0
   if (ac != 2) {
-     printf("Usage: %s filename\n", av[0]);
+    printf("Usage: %s filename\n", av[0]);
   }
-  FILE* file; // rbp-0x80
-  FILE* backup; // rbp-0x88
+  int fd;
+  char dest[104];
+  char ret;
+  FILE *file;   // rbp-0x80
+  FILE *backup; // rbp-0x88
   int i;
 
   backup = fopen("./backups/.log", "r");
   if (backup == NULL) {
-    printf("ERROR: Failed to open %s\n", "./backukps/.log");
+    printf("ERROR: Failed to open %s\n", "./backups/.log");
     exit(1);
   }
   log_wrapper(backup, "Starting back up: ", av[1]);
@@ -20,5 +35,20 @@ int main(int ac, char **av) { // rbp-0x94 and rbp-0xa0
     printf("ERROR: Failed to open %s\n", av[1]);
     exit(1);
   }
-  
+  strcpy(dest, "./backups/");
+  strncat(dest + 10, av[1], strlen(av[1]));
+  fd = open(dest, 193, 432);
+  if (fd < 0) {
+    printf("ERROR: Failed to open %s%s\n", "./backups/", av[1]);
+    exit(1);
+  }
+  while (1) {
+    ret = fgetc(file);
+    if (ret == -1)
+      break;
+    write(fd, &ret, 1);
+  }
+  log_wrapper(backup, "Finished back up ", av[1]);
+  fclose(file);
+  close(fd);
 }
